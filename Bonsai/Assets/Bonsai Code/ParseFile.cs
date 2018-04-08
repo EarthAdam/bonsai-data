de66366623c882826a5296c9796dc2fade53e309
Adam Sauer <adamsauer.ee@gmail.com>
2018-04-08 01:41:36 -0400

    Played with the example method a little bit, and added directory log (disk usage) for Unity folder

---
+++ Bonsai/Assets/Bonsai Code/ParseFile.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+
+public class ParseFile : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        ParseTxtFile();
+	}
+    public void ParseTxtFile()
+    {
+        string text = File.ReadAllText("Assets/Bonsai Code/combined.txt");
+
+        char[] separators = { ',', ';', '|' };
+        string[] strValues = text.Split(separators);
+
+        List<string> stringValues = new List<string>();
+        foreach (string str in strValues)
+        {
+                stringValues.Add(str);
+                Debug.Log(str);
+          
+
+        }
+    }
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
