fe0d4c235b9e400772c98448a1f5f4f986a5deed
Adam Sauer <adamsauer.ee@gmail.com>
2018-04-09 14:37:28 -0400

    Make each file path thickness logarithmically scaled based on file size

--- Bonsai/Assets/Bonsai Code/ParseFile.cs
+++ Bonsai/Assets/Bonsai Code/ParseFile.cs
@@ -5,38 +5,44 @@ using System.IO;
 using System;
 
 public class ParseFile : MonoBehaviour {
-	// Use this for initialization
-	void Start () {
+    private int maxLevels = 0;
+    // Use this for initialization
+    public void Start () {
         ParseTxtFile();
 	}
     public void ParseTxtFile()
     {
         string text = File.ReadAllText("../output2.txt");
-        char[] separators = { ',', ';', '|','\n' };
+        char[] separators = { ',', ';', '|', '\n' };
         string[] strValues = text.Split(separators);
         int[] sizes = new int[strValues.Length];
         List<string> stringValues = new List<string>();
+        
+
         for (int i = 0; i < strValues.Length; i++)
         {
             string[] lineValues = strValues[i].Split('\t');           //Break up string by tab separators
             sizes[i] = Convert.ToInt32(lineValues[0]);                //Save file size
-            string[] fileString = lineValues[1].Split('/');            //break apart file path by '/' marks
-            int level = fileString.Length;                            //count the number of folders and assign each file a level
+            string[] fileString = lineValues[1].Split('/');           //break apart file path by '/' marks
+            if (fileString.Length > maxLevels)
+                maxLevels = fileString.Length;
+            //assign each file a level based on how many folders it's in
+            //count the number of repeated folders in each level
             GameObject lineObject = new GameObject(lineValues[1]);
             LineRenderer fileBranch = lineObject.AddComponent<LineRenderer>();
-            fileBranch.startWidth = 0.2f;
-            fileBranch.endWidth = 0.2f;
-            fileBranch.SetVertexCount(level);
+            fileBranch.startWidth = Mathf.Log10(sizes[i])/10;
+            fileBranch.endWidth = Mathf.Log10(sizes[i])/10;
+            fileBranch.SetVertexCount(fileString.Length);
             fileBranch.SetPosition(0, new Vector3(0, 0, 0));
-            for (int j = 1; j < level; j++)
+            fileBranch.SetPosition(1, new Vector3(0, strValues.Length / 100, 0));
+            for (int j = 2; j < fileString.Length; j++)
             {
                 fileBranch.SetPosition(j, new Vector3(i / 10 - strValues.Length / 2 / 10, strValues.Length / 100*j, 0));
             }
-            
 
             //Debug.Log(fileString[0]+','+fileString[1] + ',' + fileString[2]);
             Debug.Log(sizes[i]);
-            Debug.Log(level);
+            Debug.Log(fileString.Length);
             //Next: count the number of times a folder path is repeated for each level
         }
         foreach (string str in strValues)
