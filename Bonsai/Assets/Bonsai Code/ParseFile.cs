82ce9c71bb79508d159be0db3f942f43b64c209d
Adam Sauer <adamsauer.ee@gmail.com>
2018-05-09 22:53:17 -0400

    Trying to go back to the old ways

--- Bonsai/Assets/Bonsai Code/ParseFile.cs
+++ Bonsai/Assets/Bonsai Code/ParseFile.cs
@@ -11,41 +11,43 @@ public class ParseFile : MonoBehaviour {
     public float connectedNodeForce = 1;
     public float disconnectedNodeForce = 1;
     public List<Node> nodes;
-    public GameObject nodePrefab;
     // Use this for initialization
     public void Start () {
         nodes = new List<Node>();
         ParseTxtFile();
-        PopulateNodes();
-        //FindParents();
-    }
+	}
     // Update is called once per frame
     void Update()
     {
+        //ApplyGraphForce();
+        //foreach (var node in nodes)
+        //{
+        //    node.position += node.velocity * Time.deltaTime;
+        //}
     }
     void ParseTxtFile()
     {
-        string text = File.ReadAllText("../docs/output.txt");
+        string text = File.ReadAllText("../output.txt");
         char[] separators = { ',', ';', '|', '\n' };
         string[] strValues = text.Split(separators);
         int[] sizes = new int[strValues.Length];
         List<string> folders = new List<string>();
-        for (int i = 0; i < strValues.Length-1; i++)
+        for (int i = 0; i < strValues.Length; i++)
         {
-            //Debug.Log("Entering loop #: "+i+1);
             string[] lineValues = strValues[i].Split('\t');                 //Break up string by tab separators
             sizes[i] = Convert.ToInt32(lineValues[0]);                      //Save file size
-            string[] fileString = lineValues[1].Split('/');                 //break apart file path by '/' marks
+            string[] fileString = lineValues[1].Split('/');
+            string[] parentFolder = new string[fileString.Length];          //Creates an array for the parent folder
+            Array.Copy(fileString, parentFolder, fileString.Length - 1);
+            /*//break apart file path by '/' marks
             if (fileString.Length == 1)
             {
                 nodes.Add(new Node()
                 {
-                    pathName = lineValues[1],
-                    parentName = "Base",
-                    position = new Vector3(0, 0, 0)
+                    pathName = string.Join("", fileString),
+                    position = new Vector3(0, 0, 0),
+                    velocity = Vector3.zero
                 });
-                //Debug.Log(nodes[i].pathName + ',' + nodes[i].parentName);
-                //Debug.Log(fileString.Length);
             }
             else
             {
@@ -53,80 +55,138 @@ public class ParseFile : MonoBehaviour {
                 Array.Copy(fileString, parentFolder, fileString.Length - 1);    //Copies everything before the last '/' in the file path
                 nodes.Add(new Node()
                 {
-                    pathName = lineValues[1],
-                    parentName = string.Join("/", parentFolder).TrimEnd('/'),
-                    position = UnityEngine.Random.insideUnitSphere * 10 + new Vector3(0, 5, 0)
+                    pathName = string.Join("", fileString),
+                    parentName = string.Join("", parentFolder),
+                    position = UnityEngine.Random.insideUnitSphere * 10 + new Vector3(0, 5, 0),
+                    velocity = Vector3.zero,
                     //children = string.Join("", parentFolder).ToList()
                 });
                 Debug.Log(nodes[i].pathName+','+nodes[i].children);
-            }
-        }
-    }
-    public void AddBranch(GameObject prefab, string branchName, string parentName, Transform branchTX)
-    {
-        Instantiate(prefab,branchTX);
-        //prefab.GetComponent<SpringJoint>().connectedBody = GameObject.Find(parentName).GetComponent<Rigidbody>();
-        //prefab.GetComponent<LineRenderer>().SetPosition(0,GameObject.Find(parentName).GetComponent<Rigidbody>().transform.position);
-    }
-    void PopulateNodes()
-    {
-        foreach (var node in nodes)
-        {
-            GameObject newBranch = Instantiate(nodePrefab);
-            newBranch.name = node.pathName;
-            newBranch.GetComponent<Forces>().parentName = node.parentName;
-            newBranch.GetComponent<Forces>().parent = GameObject.Find(node.parentName);
-            //newBranch.GetComponent<SpringJoint>().connectedBody = GameObject.Find(node.parentName).GetComponent<Rigidbody>();
-            //AddBranch(nodePrefab,node.pathName,node.parentName,node.sphere.transform);
-        }
-    }/*
-    void FindParents()
-    {
-        Debug.Log("The FindParents() script started");
-        foreach (var node in nodes)
-        {
-            if (node.parentName != "base")
+            }*/
+            GameObject lineObject = new GameObject(lineValues[1]);
+            LineRenderer fileBranch = lineObject.AddComponent<LineRenderer>();
+            fileBranch.startWidth = Mathf.Log10(sizes[i]) / 10;
+            fileBranch.endWidth = Mathf.Log10(sizes[i]) / 10;
+            fileBranch.SetVertexCount(fileString.Length);
+            fileBranch.SetPosition(0, new Vector3(0, 0, 0));
+            fileBranch.SetPosition(1, new Vector3(0, strValues.Length / 1000, 0));
+            Renderer rend = fileBranch.GetComponent<Renderer>();
+            if (lineValues[1].Substring(lineValues[1].Length - 3) == "png" || lineValues[1].Substring(lineValues[1].Length - 3) == "jpg")
             {
-                Debug.Log("This node is not the base");
-                LineRenderer branch = node.sphere.AddComponent<LineRenderer>();
-                branch.SetPosition(0, node.sphere.transform.position);
-                branch.SetPosition(1, new Vector3(0,0,0));
-                //node.parent = GameObject.Find(node.parentName).GetComponent<Rigidbody>();
-                Debug.Log("Line Added");
-                Debug.Log(node.pathName);
-                Debug.Log(node.parentName);
-                String tempName = node.parentName;
-                //IEnumerable<Node> localParent = nodes.Where(node => node.pathName == tempName);
-                Debug.Log("This is what I think should be the parent name: "+ nodes.Where(i => i.pathName == node.parentName));
-                //Debug.Log("This is what I think should be the parent name: " + nodes.Find(j => j.pathName == node.parentName).parent);
-
-                //SpringJointlocal node.sphere.GetComponent<SpringJoint>().connectedBody = nodes.Find(i => i.pathName == node.parentName).sphere.GetComponent<Rigidbody>();
-                SpringJoint localSpring = node.sphere.AddComponent<SpringJoint>();
-                Debug.Log("SpringJoint Added");
-
-                //node.sphere.GetComponent<SpringJoint>().connectedBody = nodes.Find(i => i.pathName == node.parentName).sphere.GetComponent<Rigidbody>();
-                Debug.Log("I think we've successfully found the node's SpringJoint");
-                //localSpring.connectedBody.position = nodes.Where(i => i.pathName == node.parentName).transform.position;// GetComponent<Rigidbody>();
-                
-                //Debug.Log("Parent node was found");
-                
-                //Debug.Log(node.pathName + " parent found");
+                rend.material.color = new Color(1f, 1f, 0f);
+                rend.material.SetColor("_EmissionColor", new Color(1f, 1f, 0f));
+            }
+            else if (lineValues[1].Substring(lineValues[1].Length - 3) == "txt" || lineValues[1].Substring(lineValues[1].Length - 3) == ".md")
+            {
+                rend.material.color = new Color(0.1f, 0.1f, 1f);
+                rend.material.SetColor("_EmissionColor", new Color(0.1f, 0.1f, 1f));
+            }
+            else if (lineValues[1].Substring(lineValues[1].Length - 3) == ".cs")
+            {
+                rend.material.color = new Color(0.3f, 1f, 1f);
+                rend.material.SetColor("_EmissionColor", new Color(0.1f, 1f, 1f));
+            }
+            else if (lineValues[1].Substring(lineValues[1].Length - 4) == "meta" )
+            {
+                rend.material.color = new Color(1f, 1f, 1f);
+                rend.material.SetColor("_EmissionColor", new Color(1f, 1f, 1f));
+                fileBranch.startWidth = Mathf.Log10(sizes[i]) / 10;
+                fileBranch.endWidth = Mathf.Log10(sizes[i]) / 10;
+            }
+            else if (lineValues[1].Substring(lineValues[1].Length - 3) == "mat")
+            {
+                rend.material.color = new Color(1f, 0f, 0f);
+                rend.material.SetColor("_EmissionColor", new Color(1f, 0f, 0f));
+            }
+            else if (lineValues[1].Substring(lineValues[1].Length - 3) == "fab")
+            {
+                rend.material.color = new Color(1f, 1f, 0f);
+                rend.material.SetColor("_EmissionColor", new Color(1f, 1f, 0f));
+            }
+            else if (lineValues[1].Substring(lineValues[1].Length - 3) == "fbx")
+            {
+                rend.material.color = new Color(0,0,3);
+                rend.material.SetColor("_EmissionColor", new Color(0,0,3));
+            }
+            else if (lineValues[1].Substring(lineValues[1].Length - 3) == "exe")
+            {
+                rend.material.color = new Color(0f, 1f, 1f);
+                rend.material.SetColor("_EmissionColor", new Color(0f, 1f, 1f));
+            }
+            else if (lineValues[1].Substring(lineValues[1].Length - 3) == "set")
+            {
+                rend.material.color = new Color(1f, 1f, 1f);
+                rend.material.SetColor("_EmissionColor", new Color(1f, 1f, 1f));
+            }
+            else if (lineValues[1].Substring(lineValues[1].Length - 3) == "wav")
+            {
+                rend.material.color = new Color(1f, 0f, 1f);
+                rend.material.SetColor("_EmissionColor", new Color(1f, 0f, 1f));
+            }
+            else if (lineValues[1].Substring(lineValues[1].Length - 4) == "info")
+            {
+                rend.material.color = new Color(0.7f, 1f, 1f);
+                rend.material.SetColor("_EmissionColor", new Color(0.7f, 1f, 1f));
+            }
+            else if (lineValues[1].Substring(lineValues[1].Length - 5) == "unity")
+            {
+                rend.material.color = new Color(0.7f, 1f, 1f);
+                rend.material.SetColor("_EmissionColor", new Color(0.7f, 1f, 1f));
+            }
+            else if (lineValues[1].Substring(lineValues[1].Length - 5) == "cache")
+            {
+                rend.material.color = new Color(0.7f, 0.7f, 1f);
+                rend.material.SetColor("_EmissionColor", new Color(0.7f, 0.7f, 1f));
             }
             else
             {
-                Debug.Log("This node is the base");
-                Debug.Log(node.pathName);
-                Debug.Log(node.parentName);
+                rend.material.color = new Color(0.2f, 0.2f, 0.2f);
+                rend.material.SetColor("_EmissionColor", new Color(0.2f, 0.2f, 0.2f));
+            }
+            //rend.material.SetColor("_EmissionColor", color1);
+            for (int j = 2; j < fileString.Length; j++)
+            {
+                if(lineValues[1].Substring(lineValues[1].Length - 4) == "meta")
+                {
+                    fileBranch.SetPosition(j, new Vector3(i / 100 - strValues.Length / 2 / 100, strValues.Length / 1000 * j, 20));
+                }
+                else 
+                {
+                    fileBranch.SetPosition(j, new Vector3(i / 100 - strValues.Length / 2 / 100, strValues.Length / 1000 * j, 0));
+                }
+
+                //fileBranch.SetPosition(j, new Vector3(
+                //   100 * Mathf.Sin(i * 4 * Mathf.PI / strValues.Length),
+                //   strValues.Length / 1000 * j,
+                //   100 * Mathf.Cos(i * 4 * Mathf.PI / strValues.Length)
+                //));
             }
         }
-    }*/
+        /*
+        foreach(var node in nodes)
+        {
+            node.children = nodes.Where(node.pathName==node.parentName)
+        }
+        for (int i = 0; i < strValues.Length; i++)
+        {
+            string[] fileString = nodes[i].pathName.Split('/');                 //break apart file path by '/' marks
+            string[] parentFolder = new string[fileString.Length];          //Creates an array for the parent folder
+            Array.Copy(fileString, parentFolder, fileString.Length - 1);
+            foreach(var node in nodes)
+            {
+                if (nodes[i].pathName == node.parentName)
+                    nodes[i].children = node.parentName;// s.Where(node => node.pathName == string.Join("", parentFolder)).ToList();
+            }
+        }*/
+    }
+    
 }
 public class Node
 {
-    public GameObject sphere;
-    public GameObject parent;
     public string pathName;
     public string parentName;
     public Vector3 position;
+    public Vector3 velocity;
     public List<Node> children;
+
 }
