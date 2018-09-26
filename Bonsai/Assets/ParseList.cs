934580fe6dbb6b7f2c4af6f6459c11e455ee9d1a
Adam Sauer <adamsauer.ee@gmail.com>
2018-09-25 23:13:48 -0400

    Line for each file/folder now traces down all the way to root 	modified:   Bonsai/Assets/ParseList.cs

--- Bonsai/Assets/ParseList.cs
+++ Bonsai/Assets/ParseList.cs
@@ -1,7 +1,5 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using UnityEngine;
-using System.Linq;
 using System.IO;
 using System;
 using System.Text.RegularExpressions;
@@ -11,7 +9,6 @@ public class ParseList : MonoBehaviour
     public string[] strValues;
     public List<string> folders;
     public int lineCount;
-    // Use this for initialization
     public void Start()
     {
         GameObject trunkCylinder = GameObject.CreatePrimitive(PrimitiveType.Cylinder);
@@ -26,9 +23,12 @@ public class ParseList : MonoBehaviour
         string text = File.ReadAllText("../out.txt");
         char[] separators = { ',', ';', '|', '\n' };
         strValues = text.Split(separators);
-        //Debug.Log("Pre 'MatchFolder()' script");
+        GameObject seed = new GameObject();
+        LineRenderer seedLine = seed.AddComponent<LineRenderer>();
+        seedLine.SetVertexCount(1);
+        seedLine.SetPosition(0, new Vector3(0, 0, 0));
+        seed.name = "./";
         GrowLocalTree(MatchFolder(".:"));
-        //Debug.Log("Folders: " + folders);
         int k = 0;
         for (int k2 = k; k2 < folders.Count; k2++)
         {
@@ -38,45 +38,48 @@ public class ParseList : MonoBehaviour
     }
     int MatchFolder(string root)
     {
-        //Debug.Log("Start 'MatchFolder()' script");
         bool match = false;
         int lineNumber=0;
         for (int i = 0; match == false; i++)
         {
-            //Debug.Log("'MatchFolder()' match for loop");
-            //Debug.Log("'MatchFolder()' pre match found condition. i = " + i + ", strValues[i] = " + strValues[i] + ", root = " + root);
             if (strValues[i] == root)
             {
                 match = true;
                 lineNumber = i;
-                //Debug.Log("'MatchFolder()' match found condition. i = "+i+", strValues[i] = "+ strValues[i]+", root = "+root);
             }
         }
-        //Debug.Log("Pre 'MatchFolder()' Return lineNumber");
         return lineNumber;
     }
-    void GrowLocalTree(int lineNumber)
+    int SetFileSize(int lineNumber)
+    {
+        int size;
+        if (Int32.Parse(Regex.Match(strValues[lineNumber + 1], @"\d+").Value) != 0)
+            size = Int32.Parse(Regex.Match(strValues[lineNumber + 1], @"\d+").Value);
+        else
+            size = 1;
+        return size;
+    }
+    int SetScale(int lineNumber)
     {
         int scale = 1;
         if (lineNumber == 0)
             scale = 3;
         else
             scale = 1;
-        int size;
-        if (Int32.Parse(Regex.Match(strValues[lineNumber + 1], @"\d+").Value) != 0)
-        {
-            size = Int32.Parse(Regex.Match(strValues[lineNumber + 1], @"\d+").Value);
-        }
-        else
-            size = 1;
+        return scale;
+    }
+    void GrowLocalTree(int lineNumber)
+    {
+        int scale = SetScale(lineNumber);
+        int size = SetFileSize(lineNumber);
+        string parent = strValues[lineNumber].TrimEnd(':');
+        Debug.Log("parent: " + parent.TrimEnd(':'));
         GameObject trunkCylinder = GameObject.Find(strValues[lineNumber]);
-        //Debug.Log("Trunk name is: " + strValues[lineNumber]);
         Boolean blankLine = false;
         lineCount = lineNumber + 2;
         int taken = 0;
         while (blankLine == false)
         {
-            //Debug.Log("water:" + strValues[lineCount]);
             if (strValues[lineCount] == "")
             {
                 blankLine = true;
@@ -84,27 +87,27 @@ public class ParseList : MonoBehaviour
             else
             {
                 string slightlyShorterLine = strValues[lineCount].TrimStart(' ');
-                //Debug.Log("slightlyShorterLine: " + slightlyShorterLine);
                 string[] lineValues = slightlyShorterLine.Split(' ');
                 int fileSize = Int32.Parse(lineValues[0]);
-                //Debug.Log("fileSize: "+fileSize);
                 string fileName_ws = slightlyShorterLine.TrimStart(lineValues[0].ToCharArray());
                 string fileName = fileName_ws.TrimStart(' ');
-                //Debug.Log("fileName: " + fileName);
-
-
-                //int fileSize = Int32.Parse(lineValues[lineValues.Length - 2]);
-                //int fileSize = Int32.Parse(lineValues[lineValues.Length - 2]);
-                //string fileName = lineValues[lineValues.Length - 1];
-                //Debug.Log("Size = " + fileSize + ", Name: " + fileName);
+                Debug.Log("FileName: " + fileName);
                 taken += fileSize;
                 int range = taken - (taken - fileSize);
                 float angle = taken - (float)range / 2;
-                //Debug.Log("Range = " + range + "Angle = " + angle);
-
-                GameObject lineObject = new GameObject(fileName);
-                LineRenderer fileBranch = lineObject.AddComponent<LineRenderer>();
-                fileBranch.SetVertexCount(3);
+                //
+                // Basically grab the parent folder name, and use that to look up a line with that name
+                // 
+                // Then copy that line, and append more points to it
+                // 
+                // Then rename it to include the fileName
+                //
+                // Then maybe add this "size" to the parent "size"?
+                //
+                GameObject lineObject = Instantiate(GameObject.Find(parent+"/"));
+                lineObject.name = parent + "/" + fileName;
+                Debug.Log("Line Name = " + lineObject.name);
+                LineRenderer fileBranch = lineObject.GetComponent<LineRenderer>();
                 if (fileSize == 0)
                 {
                     fileBranch.startWidth = 0.25f;
@@ -115,9 +118,9 @@ public class ParseList : MonoBehaviour
                     fileBranch.startWidth = Mathf.Log10(fileSize) / 1;
                     fileBranch.endWidth = Mathf.Log10(fileSize) / 1;
                 }
-                fileBranch.SetPosition(0, trunkCylinder.transform.position);
-                fileBranch.SetPosition(1, trunkCylinder.transform.position + new Vector3(0, scale * 30,0));
-                fileBranch.SetPosition(2, trunkCylinder.transform.position + new Vector3(
+                fileBranch.SetVertexCount(fileBranch.positionCount+2);
+                fileBranch.SetPosition(fileBranch.positionCount-2, fileBranch.GetPosition(fileBranch.positionCount-3) + new Vector3(0, scale * 30,0));
+                fileBranch.SetPosition(fileBranch.positionCount-1, fileBranch.GetPosition(fileBranch.positionCount-3) + new Vector3(
                     scale * 50 * Mathf.Cos(angle * 360 / size * Mathf.PI / 180),
                     scale * 50,
                     scale * 50 * Mathf.Sin(angle * 360 / size * Mathf.PI / 180)
@@ -142,8 +145,6 @@ public class ParseList : MonoBehaviour
                 }
             }
             lineCount++;
-            //Debug.Log("Near the end. Here's the lineCount: " + lineCount);
         }
-    }
-        
+    }        
 }
